f7b618f8100613dfdc8bceae29f29293770518a8
Abdelrhman Shawky <abdelrhmanshawky4@gmail.com>
2017-04-18 18:25:36 +0200

    Initial commit

---
+++ PunycodePreventerChrome/app/phishingCheck.js
@@ -0,0 +1,10 @@
+debugger;
+var url = document.URL;
+var decodedUrl = punycode.toUnicode(url);
+if (url !== decodedUrl) {
+        console.log('Possible Phishing');
+        var div = document.createElement("div");
+        document.body.insertBefore(div, document.body.childNodes[0]);
+        div.style = "background-color: #540000; color: white;";
+        div.innerText = "This website seems to be unsafe. The reason we believe that is because the url is using punycode. Please note that this website is "+url+ " not "+decodedUrl+ ".";
+    }
